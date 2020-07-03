d2dc20fefd770a488af91b329626ae5494a49b38
jarrodmcorrell <67766756+jarrodmcorrell@users.noreply.github.com>
2020-07-02 23:30:23 -0400

    add new stuff

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
-print("What would you like to do?")
+import getpass
+import os
+os.system('cls')
+
+file1 = open("accounts.txt", "r+")
+
+
+print(file1.read())
+
+print("")
+print("")
+print("Welcome to Financial Pro!")
 print("-----------")
 print("Login (1)")
 print("Create Account (2)")
 print("-----------")
-print("---Login---")
-print("Enter Username: ")
-print("Enter Password: ")
-print("---Create Account---")
-print("Enter Username: ")
-print("Enter Password: ")
+
+choice = input("$: ")
+if choice=="1":
+    os.system('cls')
+    print("---Login---")
+    print("Enter Username: ")
+    loguser = input("$: ")
+    print("Enter Password: ")
+    logpass = getpass.getpass("$: ")
+
+
+elif choice == "2":
+    os.system('cls')
+    print("---Create Account---")
+    print("Enter Username: ")
+    createuser = input("$: ")
+    print("Enter Password: ")
+    createpass = getpass.getpass("$: ")
+    print("ReEnter Password: ")
+    createpass2 = getpass.getpass("$: ")
+    if createpass == createpass2:
+        print("success!")
+    file1.write(createuser + "\n")
+    file1.write(createpass + "\n")
+    print (file1.readline(0))
+
+else:
+    print("error")
+
+
+
+
+
+
+
+
+
+
+
